b58cc07c462f0635468d613d6b43f6d822b55507
Ezra Moore <Ezra.Moore@guard.com>
2020-01-10 15:22:19 -0500

    further updates

--- controllers/product.js
+++ controllers/product.js
@@ -15,9 +15,22 @@ exports.create = (req, res) => {
             });
         }
 
+        //checks for all fields
+        const { name, desc, price, category, quantity } = fields;
+        if (!name || !desc || !price || !category || !quantity) {
+            return res.status(400).json({
+                error: "All fields required"
+            });
+        }
+
         let product = new Product(fields);
 
         if (files.photo) {
+            if (files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: "Image cannot be larger than 1 mb"
+                });
+            }
             product.photo.data = fs.readFileSync(files.photo.path);
             product.photo.contentType = files.photo.type;
         }
